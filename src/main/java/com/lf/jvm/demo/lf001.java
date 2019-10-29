77c5042aa77e61f49ecf1bcec3e4a530d9f3ae9e
gitLFFF <%&@90046109lf>
2019-10-29 22:21:20 +0800

    this is update?

--- src/main/java/com/lf/jvm/demo/lf001.java
+++ src/main/java/com/lf/jvm/demo/lf001.java
@@ -1,9 +1,25 @@
 package com.lf.jvm.demo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class lf001 {
     private String str;
 
-    public void sys(){
-        System.out.println(str="ddffde");
+    public void sys() {
+        System.out.println(str = "ddffde");
+
     }
+
+    public static void main(String[] args) {
+        ArrayList<Object> a = new ArrayList<>();
+        a.add("ddd");
+        a.add("jack");
+        Iterator<Object> iterator = a.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+    }
+
 }
